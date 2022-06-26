cd508153d7b96334b6cdb7ca087d26b44589c8c8
letsola <92208108+Lexsolena@users.noreply.github.com>
2022-06-26 16:12:21 +0000

    Update pasteusermode.hpp

--- pasteusermode.hpp
+++ pasteusermode.hpp
@@ -1,1 +1,32 @@
 
+#pragma once
+#include <Windows.h>
+#include <stdio.h>
+#include "communication.hpp"
+
+namespace driver
+{
+	__int64(__fastcall* NtCompositionInputThread)(void* arg01, void* arg02) = nullptr;
+
+	bool setup()
+	{
+		LoadLibraryA("user32.dll");
+		LoadLibraryA("win32u.dll");
+
+		const HMODULE win32u = GetModuleHandleA("win32u.dll");
+
+		if (!win32u)
+			return false;
+
+		*(void**)&NtCompositionInputThread = GetProcAddress(win32u, "NtCompositionInputThread");
+
+		return NtCompositionInputThread;
+	}
+
+	bool send_cmd(cmd_t* cmd, void* arg02 = NULL)
+	{
+		RtlSecureZeroMemory(cmd, 0);
+		NtCompositionInputThread(cmd, arg02);
+		return cmd->success;
+	}
+}
